d233b0504f1f8aebed7c6d76afc59d02e567f13d
adeelfeb <at4563323@gmail.com>
2024-12-08 12:53:37 +0500

    ✨ Add examples exploring the `this` keyword in JavaScript
    - Demonstrated `this` behavior in regular and arrow functions.
    - Showcased `call`, `bind`, and borrowing methods.
    - Included examples for `this` in constructors and event listeners.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hisdfsdf")
+
+const obj = {
+    x: 30,
+    funcc: function(){
+        const nested = ()=>{
+            console.log("Int heneseed")
+            console.log(this)
+        }
+        nested()
+    }
+}
+
+
+const obj2 ={
+    x: 45,
+    y:2
+}
+
+obj.funcc()
+obj.funcc.call(obj2)
